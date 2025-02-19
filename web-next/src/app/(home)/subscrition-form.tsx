59cd8b9bc6ecf8d9a620e186d2fd0616572b2a1d
marlisonmourao <bentesmourao@gmail.com>
2025-02-18 23:04:16 -0400

    feat: integrate react-hook-form and zod for subscription form validation; update invite link input component

--- web-next/src/app/(home)/subscrition-form.tsx
+++ web-next/src/app/(home)/subscrition-form.tsx
@@ -1,30 +1,81 @@
+'use client'
+
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const subscriptionSchema = z.object({
+  name: z.string().min(1, 'Digite seu nome completo'),
+  email: z.string().email('Informe um e-email válido'),
+})
+
+type SubscriptionFormData = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionFormData>({
+    resolver: zodResolver(subscriptionSchema),
+  })
+
+  function onSubscribed(data: SubscriptionFormData) {
+    console.log(data)
+  }
+
   return (
-    <form className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]">
+    <form
+      onSubmit={handleSubmit(onSubscribed)}
+      className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]"
+    >
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
         Incrição
       </h2>
 
       <div className="space-y-3">
-        <InputRoot>
-          <InputIcon>
-            <User />
-          </InputIcon>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User />
+            </InputIcon>
 
-          <InputField type="text" placeholder="Nome completo" />
-        </InputRoot>
+            <InputField
+              type="text"
+              placeholder="Nome completo"
+              {...register('name')}
+            />
+          </InputRoot>
 
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
+          {errors.name && (
+            <span className="text-danger text-xs font-semibold">
+              {errors.name.message}
+            </span>
+          )}
+        </div>
 
-          <InputField type="email" placeholder="E-mail" />
-        </InputRoot>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+
+            <InputField
+              type="email"
+              placeholder="E-mail"
+              {...register('email')}
+            />
+          </InputRoot>
+
+          {errors.email && (
+            <span className="text-danger text-xs font-semibold">
+              {errors.email.message}
+            </span>
+          )}
+        </div>
       </div>
 
       <Button type="submit">
--- web-next/src/app/invite/invite-link-input.tsx
+++ web-next/src/app/invite/invite-link-input.tsx
@@ -1,17 +1,27 @@
+'use client'
+
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyToClipboard() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
 
-      <InputField readOnly defaultValue="http://locahost:3000/invite" />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyToClipboard}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
